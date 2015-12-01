7344e720cfd419f829003a54b607f74d188be5fa
Roland Kaiser <roli@Zora.fritz.box>
2015-12-01 20:48:24 +0100

    time for a new version 0.1-2

--- install/BuildAndInstall.sh
+++ install/BuildAndInstall.sh
@@ -1,2 +1,2 @@
 R CMD build --no-build-vignettes /Users/roli/Documents/synoptic/pkg
-R CMD INSTALL -l /Users/roli/Library/R/3.2/library synoptic_0.1-1.tar.gz
+R CMD INSTALL -l /Users/roli/Library/R/3.2/library synoptic_0.1-2.tar.gz
