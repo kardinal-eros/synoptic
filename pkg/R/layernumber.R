45837b67366b3b10eb748712d5f49d365c4a0f07
Roland Kaiser <roli@Zora.fritz.box>
2015-12-01 20:15:44 +0100

    moved functions to package vegsoup

--- pkg/R/layernumber.R
+++
@@ -1,2 +0,0 @@
-#	Return layer as ordered integer
-layernumber <- function (obj) as.numeric(ordered(splitAbbr(obj)$layer, layers(obj)))
--- pkg/R/partitions.R
+++
@@ -1,7 +0,0 @@
-#	number of samples per cluster
-partitions <- function (obj) {
-	r <- as.vector(table(partitioning(obj)))
-	names(r) <- 1:getK(obj)
-	return(r)
-}
-
